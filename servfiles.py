a5692475aa862c00037114b2114a9b8f99ec6949
yongfei <yongfei@Gmail.com>
2023-03-01 20:57:51 -1000

    Add files via upload

--- servfiles.py
+++ servfiles.py
@@ -62,34 +62,6 @@ def get_track_image(artist, album):
         ]
     except (KeyError, IndexError):
         return blank_image
-"""
-@app.route('/music', defaults={'req_path': ''})
-@app.route('/<path:req_path>')
-def dir_listing(req_path):
-    BASE_DIR = '.'
-
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        return abort(404)
-
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-
-        #return send_file(abs_path)
-        uri='http://192.168.68.128:8081/'+abs_path.lstrip('./music/')
-        print(uri)
-        sonos.play_uri(uri)
-        return "Ok"
-
-
-    # Show directory contents
-    files = os.listdir(abs_path)
-    return render_template('files.html', files=files)
-
-"""
 @app.route("/play")
 def play():
     sonos.play()
