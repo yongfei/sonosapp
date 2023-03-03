10e67b1104f682f132c1da4211ad767717af96ed
yongfei <yongfei@Gmail.com>
2023-03-02 20:16:03 -1000

    Add files via upload

--- index.py
+++ index.py
@@ -19,51 +19,11 @@ app.config.from_pyfile("settings.py")
 sonos = SoCo(app.config["SPEAKER_IP"])
 
 
-def gen_sig():
-    return hashlib.md5(
-        (
-            app.config["ROVI_API_KEY"]
-            + app.config["ROVI_SHARED_SECRET"]
-            + repr(int(time.time()))
-        ).encode("utf-8")
-    ).hexdigest()
-
-
-def get_track_image(artist, album):
-    blank_image = url_for("static", filename="img/blank.jpg")
-    if "ROVI_SHARED_SECRET" not in app.config:
-        return blank_image
-    elif "ROVI_API_KEY" not in app.config:
-        return blank_image
-
-    headers = {"Accept-Encoding": "gzip"}
-    req = requests.get(
-        "http://api.rovicorp.com/recognition/v2.1/music/match/album?apikey="
-        + app.config["ROVI_API_KEY"]
-        + "&sig="
-        + gen_sig()
-        + "&name= "
-        + album
-        + "&performername="
-        + artist
-        + "&include=images&size=1",
-        headers=headers,
-    )
-
-    if req.status_code != requests.codes.ok:
-        return blank_image
-
-    result = json.loads(req.content)
-    try:
-        return result["matchResponse"]["results"][0]["album"]["images"][0]["front"][3][
-            "url"
-        ]
-    except (KeyError, IndexError):
-        return blank_image
-@app.route('/music', defaults={'req_path': ''})
+#@app.route("/")
+@app.route('/', defaults={'req_path': ''})
 @app.route('/<path:req_path>')
 def dir_listing(req_path):
-    BASE_DIR = '/home/john/Downloads/'
+    BASE_DIR = '/home/john/Downloads/music'
 
     # Joining the base and the requested path
     abs_path = os.path.join(BASE_DIR, req_path)
@@ -90,48 +50,6 @@ def dir_listing(req_path):
     # Show directory contents
     files = os.listdir(abs_path)
     return render_template('files.html', files=files)
-
-@app.route("/play")
-def play():
-    sonos.play()
-    return "Ok"
-
-@app.route("/pause")
-def pause():
-    sonos.pause()
-    return "Ok"
-
-
-@app.route("/next")
-def next():
-    sonos.next()
-    return "Ok"
-
-
-@app.route("/previous")
-def previous():
-    sonos.previous()
-    return "Ok"
-
-
-@app.route("/info-light")
-def info_light():
-    track = sonos.get_current_track_info()
-    return json.dumps(track)
-
-
-@app.route("/info")
-def info():
-    track = sonos.get_current_track_info()
-    track["image"] = get_track_image(track["artist"], track["album"])
-    return json.dumps(track)
-
-
-@app.route("/")
-def index():
-    track = sonos.get_current_track_info()
-    track["image"] = get_track_image(track["artist"], track["album"])
-    return render_template("index.html", track=track)
 
 
 if __name__ == "__main__":
--- servfiles.py
+++ servfiles.py
@@ -21,94 +21,11 @@ sonos = SoCo(app.config["SPEAKER_IP"])
 
 files_index = AutoIndex(app, browse_root='/home/john/Downloads/music/', add_url_rules=False)
 
-def gen_sig():
-    return hashlib.md5(
-        (
-            app.config["ROVI_API_KEY"]
-            + app.config["ROVI_SHARED_SECRET"]
-            + repr(int(time.time()))
-        ).encode("utf-8")
-    ).hexdigest()
-
-
-def get_track_image(artist, album):
-    blank_image = url_for("static", filename="img/blank.jpg")
-    if "ROVI_SHARED_SECRET" not in app.config:
-        return blank_image
-    elif "ROVI_API_KEY" not in app.config:
-        return blank_image
-
-    headers = {"Accept-Encoding": "gzip"}
-    req = requests.get(
-        "http://api.rovicorp.com/recognition/v2.1/music/match/album?apikey="
-        + app.config["ROVI_API_KEY"]
-        + "&sig="
-        + gen_sig()
-        + "&name= "
-        + album
-        + "&performername="
-        + artist
-        + "&include=images&size=1",
-        headers=headers,
-    )
-
-    if req.status_code != requests.codes.ok:
-        return blank_image
-
-    result = json.loads(req.content)
-    try:
-        return result["matchResponse"]["results"][0]["album"]["images"][0]["front"][3][
-            "url"
-        ]
-    except (KeyError, IndexError):
-        return blank_image
-@app.route("/play")
-def play():
-    sonos.play()
-    return "Ok"
-
-@app.route("/music")
-@app.route("/music/")
-@app.route('/music/<path:path>')
-def autoindex(path='.'):
-    return files_index.render_autoindex(path, template='template.html')
-
-@app.route("/pause")
-def pause():
-    sonos.pause()
-    return "Ok"
-
-
-@app.route("/next")
-def next():
-    sonos.next()
-    return "Ok"
-
-
-@app.route("/previous")
-def previous():
-    sonos.previous()
-    return "Ok"
-
-
-@app.route("/info-light")
-def info_light():
-    track = sonos.get_current_track_info()
-    return json.dumps(track)
-
-
-@app.route("/info")
-def info():
-    track = sonos.get_current_track_info()
-    track["image"] = get_track_image(track["artist"], track["album"])
-    return json.dumps(track)
-
 
 @app.route("/")
-def index():
-    track = sonos.get_current_track_info()
-    track["image"] = get_track_image(track["artist"], track["album"])
-    return render_template("index.html", track=track)
+@app.route('/<path:path>')
+def autoindex(path='.'):
+    return files_index.render_autoindex(path, template='template.html')
 
 
 if __name__ == "__main__":
